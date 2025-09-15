bf9001eb13677d3d07de00b060ef29f09ab2a300
ROMULO REIS DE OLIVEIRA <01421513005@senacrs.edu.br>
2025-09-15 20:00:21 -0300

    add scaffold product

---
+++ SenacLojas/Data/SenacLojasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SenacLojas.Models;
+
+namespace SenacLojas.Data
+{
+    public class SenacLojasContext : DbContext
+    {
+        public SenacLojasContext (DbContextOptions<SenacLojasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SenacLojas.Models.Product> Product { get; set; } = default!;
+    }
+}
--- SenacLojas/Program.cs
+++ SenacLojas/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SenacLojas.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SenacLojasContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SenacLojasContext") ?? throw new InvalidOperationException("Connection string 'SenacLojasContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
